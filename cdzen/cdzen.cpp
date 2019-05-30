eb5e1a337c74a2d878b9c06069199f3563edd44c
stailgot <sanchesz@list.ru>
2019-05-30 22:21:41 +0300

    conan

--- cdzen/cdzen.cpp
+++ cdzen/cdzen.cpp
@@ -1,25 +1,15 @@
 ﻿#include <array>
 #include <iostream>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-
-#include <glm/gtc/type_ptr.hpp>
-
-#include <gl/GL.h>
-#pragma comment( lib, "opengl32.lib" )
-
-//find_package( boost REQUIRED )
-//#include <boost/range/combine.hpp>
-
 
 int main()
 {
   ::glfwInit();
   auto window = ::glfwCreateWindow( 800, 600, "", {}, {} );
   ::glfwMakeContextCurrent( window );
-  ::glfwSwapInterval( 0 );
-
+  ::gladLoadGL();
   ::glfwSetWindowSizeCallback( window, []( auto _, auto... args ) { ::glViewport( 0, 0, args... ); } );
 
   while ( !::glfwWindowShouldClose( window ) )
@@ -37,22 +27,11 @@ void draw()
   glClearColor( 0.5f, 0.f, 0.f, 0.f );
   glClear( GL_COLOR_BUFFER_BIT );
   glBegin( GL_TRIANGLES );
-
-  //constexpr std::array vertices = {glm::vec2{-1.f, -1.f}, glm::vec2{0.f, 1.f}, glm::vec2{1.f, 0.f}};
-  //constexpr std::array colors   = {glm::vec3{1.f, 0.f, 0.f}, glm::vec3{0.f, 1.f, 0.f}, glm::vec3{0.f, 0.f, 1.f}};
-
-  //for ( auto [v, c] : boost::range::combine( vertices, colors ) )
-  //{
-  //  glVertex2fv( glm::value_ptr(v) );
-  //  glColor3fv( glm::value_ptr(c.get_head()) );
-  //}
-
   glVertex2f( -1.f, -1.f );
   glColor3f( 1.f, 0.f, 0.f );
   glVertex2f( 0.f, 1.f );
   glColor3f( 0.f, 1.f, 0.f );
   glVertex2f( 1.f, 0.f );
   glColor3f( 0.f, 0.f, 1.f );
-
   glEnd();
 }

687ef7ecbf1b23992eac18710b66299f496f8fa0
tulup <tulup@ascon.ru>
2021-02-17 02:53:32 +0300

    update conanfile.txt

--- update.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-conan install . -if .\props\ --build=missing -s build_type=Release -s arch=x86
-conan install . -if .\props\ --build=missing -s build_type=Debug -s arch=x86
-
-conan install . -if .\props\ --build=missing -s build_type=Release -s arch=x86_64
-conan install . -if .\props\ --build=missing -s build_type=Debug -s arch=x86_64
---
+++ update.sh
@@ -0,0 +1,6 @@
+# conan install . -if ./props/ --build=missing -s build_type=RelWithDebInfo -s arch=x86
+# conan install . -if ./props/ --build=missing -s build_type=Debug -s arch=x86
+
+# conan install . -if ./props/ --build=missing -s build_type=RelWithDebInfo -s arch=x86_64
+conan install . -if ./props/ --build=missing -s build_type=Release -s arch=x86_64
+conan install . -if ./props/ --build=missing -s build_type=Debug -s arch=x86_64
